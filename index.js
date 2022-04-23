4a56df853ad3f2afe767688a5126697d84a1a0b9
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-23 12:16:48 -0700

    Imported fetchCoordsByIP function by including it into require code top of file. Invoked fetchCoordsByIP function and passing it my public IP as a string first argument, then callback to take care of error and geo-coords. Added code within to take care of if there's an error vs if there isn't print a console.log to confirm.

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 // Requires and will run our main fetch function
 
-const { fetchMyIP} = require("./iss");
+const { fetchMyIP, fetchCoordsByIP} = require("./iss");
 
-fetchMyIP((error, ip) => {
+// Temporary code:
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   console.log("It worked! Returned IP:", ip);
+// });
+
+fetchCoordsByIP("24.80.162.169", (error, data) => {
   if (error) {
-    console.log("It didn't work!", error);
+    console.log("Geo-coordinates could not be fetched!", error);
     return;
   }
-  console.log("It worked! Returned IP:", ip);
+  console.log("It worked! Geo-coordinates: ", data);
 });
