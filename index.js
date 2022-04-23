c1a4ccd6856cfde33da436e24ba0a9368634f183
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-23 13:10:25 -0700

    Added code to invoke function a second time, this time passing in an invalid IP.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Requires and will run our main fetch function
 
-const { fetchMyIP, fetchCoordsByIP} = require("./iss");
+const { fetchCoordsByIP} = require("./iss");
 
 // Temporary code:
 // fetchMyIP((error, ip) => {
@@ -11,6 +11,7 @@ const { fetchMyIP, fetchCoordsByIP} = require("./iss");
 //   console.log("It worked! Returned IP:", ip);
 // });
 
+// Valid IP
 fetchCoordsByIP("24.80.162.169", (error, coordinates) => {
   if (error) {
     console.log("It didn't work!", error);
@@ -18,3 +19,13 @@ fetchCoordsByIP("24.80.162.169", (error, coordinates) => {
   }
   console.log("It worked! Returned coordinates:", coordinates);
 });
+
+// Invalid IP
+fetchCoordsByIP("24.80.162.290", (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log("It worked! Returned coordinates:", coordinates);
+});
+
