af67864d3e7c50e0cffc805f49064a6d07e20dff
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-23 15:43:00 -0700

    Added commented notes from Compass exercise. For futur reference while viewing code on VSCode.

--- index.js
+++ index.js
@@ -34,6 +34,18 @@ const { nextISSTimesForMyLocation } = require("./iss");
 //   console.log("It worked! ISS fly over times for your coordinates are: ", passTimes);
 // });
 
+/* 
+ * Input: 
+ *   Array of data objects defining the next fly-overs of the ISS.
+ *   [ { risetime: <number>, duration: <number> }, ... ]
+ * Returns: 
+ *   undefined
+ * Sideffect: 
+ *   Console log messages to make that data more human readable.
+ *   Example output:
+ *   Next pass at Mon Jun 10 2019 20:11:44 GMT-0700 (Pacific Daylight Time) for 468 seconds!
+ */
+
 const printPassTimes = function(passTimes) {
   for (const pass of passTimes) {
     const datetime = new Date(0);
