e6effa5d7aa94d02191a81ead7ebda8ca0360f03
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-23 13:18:00 -0700

    Deleted second invocation of fetchCoordsByIP function.

--- index.js
+++ index.js
@@ -11,17 +11,8 @@ const { fetchCoordsByIP} = require("./iss");
 //   console.log("It worked! Returned IP:", ip);
 // });
 
-// Valid IP
-fetchCoordsByIP("24.80.162.169", (error, coordinates) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
-  }
-  console.log("It worked! Returned coordinates:", coordinates);
-});
 
-// Invalid IP
-fetchCoordsByIP("24.80.162.290", (error, coordinates) => {
+fetchCoordsByIP("24.80.162.169", (error, coordinates) => {
   if (error) {
     console.log("It didn't work!", error);
     return;
