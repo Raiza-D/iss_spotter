0b859078e240f7c2df1c64f9c0706ab43ea8a97c
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-23 15:41:03 -0700

    Added code to change formatting for date output. It loops through each object within array. Takes each key-value pair and applies formatting.

--- index.js
+++ index.js
@@ -34,9 +34,19 @@ const { nextISSTimesForMyLocation } = require("./iss");
 //   console.log("It worked! ISS fly over times for your coordinates are: ", passTimes);
 // });
 
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
-  console.log("It worked!", passTimes);
+  console.log("SUCCESS!");
+  printPassTimes(passTimes);
 });
