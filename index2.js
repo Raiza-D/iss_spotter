b0e1a6cf828e3da73591928831ac1d4186264595
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-24 13:04:53 -0700

    Fixed code to call nextISSTimesForMyLocation. Copy/pasted code for printPassTimes from index.js. Added commented out import code to require printPassTimes from index.js just to try both methods to see how each works.

--- index2.js
+++ index2.js
@@ -1,8 +1,17 @@
 const { nextISSTimesForMyLocation } = require("./iss_promised");
+// const { printPassTimes } = require("./index");
+
+const printPassTimes = function (passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
 
 nextISSTimesForMyLocation()
-  .then(fetchMyIP)
-  .then(fetchCoordsByIP)
-  .then(fetchISSFlyOverTimes)
-  .then(body => console.log(body));
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  });
 
